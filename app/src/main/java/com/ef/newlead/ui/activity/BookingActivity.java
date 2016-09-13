63794f61f167471a8e77ba4673119b9daa492fa3
Razhan <ranzhang.su@gmail.com>
2016-09-13 11:06:36 +0800

    init layout

---
+++ app/src/main/java/com/ef/newlead/ui/activity/BookingActivity.java
@@ -0,0 +1,15 @@
+package com.ef.newlead.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ef.newlead.R;
+
+public class BookingActivity extends BaseActivity {
+
+    @Override
+    public int bindLayout() {
+        return R.layout.activity_booking;
+    }
+
+}
